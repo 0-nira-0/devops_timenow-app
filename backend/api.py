7ffd246d4770415e786578dccb121af46ef31362
Mykyta Oliinyk <144736223+0-nira-0@users.noreply.github.com>
2025-10-06 15:50:07 +0200

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,21 @@
-from datetime import datetime
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-def handler(request):
-    now = datetime.now()
-    return now.strftime("%H:%M:%S")
+app = FastAPI()
+
+@app.get("/task/unchecked")
+def get_unchecked_task():
+    return JSONResponse({
+        "checked": False,
+        "text": "run 100 miles"
+    })
+
+@app.get("/task/checked")
+def get_checked_task():
+    return JSONResponse({
+        "checked": True,
+        "text": "run 100 miles"
+    })
+
+# Vercel will use this
+handler = app
