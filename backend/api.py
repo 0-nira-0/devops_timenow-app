b4b8ea802276944c95923c07b17fb3b64b4fa2cd
Mykyta Oliinyk <144736223+0-nira-0@users.noreply.github.com>
2025-10-06 15:44:11 +0200

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,13 +1,5 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
 from datetime import datetime
 
-app = FastAPI()
-
-@app.get("/time", response_class=PlainTextResponse)
-def get_time():
+def handler(request):
     now = datetime.now()
     return now.strftime("%H:%M:%S")
-
-# Vercel looks for a top-level `app` variable (ASGI)
-handler = app
